e1e952d1b9ae0a92ae5b0882a4b7b27cd1050f18
splatsid <siddu_787@hotmail.com>
2016-11-19 13:35:21 +1300

    Creating project, initial commit

---
+++ Moodify/ViewModels/MenuPageViewModel.cs
@@ -0,0 +1,47 @@
+﻿using Moodify.Views;
+using System;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Moodify
+{
+	public class MenuPageViewModel
+	{
+		public ICommand GoHomeCommand { get; set; }
+		public ICommand GoSecondCommand { get; set; }
+        public ICommand GoAboutCommand { get; set; }
+        public ICommand GoLocationCommand { get; set; }
+		public MenuPageViewModel()
+		{
+			GoHomeCommand = new Command(GoHome);
+			GoSecondCommand = new Command(GoSecond);
+            GoAboutCommand = new Command(GoAbout);
+            GoLocationCommand = new Command(GoLocation);
+
+        }
+
+		void GoHome(object obj)
+		{
+            App.RootPage.Detail = new NavigationPage(new HomePage());
+			App.MenuIsPresented = false;
+		}
+
+		void GoSecond(object obj)
+		{
+            App.RootPage.Detail = new NavigationPage(new SecondPage());
+            App.MenuIsPresented = false;
+		}
+
+        void GoAbout(Object obj)
+        {
+            App.RootPage.Detail = new NavigationPage(new AboutPage());
+            App.MenuIsPresented = false;
+        }
+
+        void GoLocation(Object obj)
+        {
+            App.RootPage.Detail = new NavigationPage(new LocationPage());
+            App.MenuIsPresented = false;
+        }
+	}
+}
